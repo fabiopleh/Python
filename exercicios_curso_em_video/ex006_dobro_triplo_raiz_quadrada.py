a6fba9d134862b361a2388d7f2e257dc5d59a626
fabiopleh <fabiopleh@gmail.com>
2023-05-28 19:25:34 -0300

    Create ex006_dobro_triplo_raiz_quadrada.py

---
+++ exercicios_curso_em_video/ex006_dobro_triplo_raiz_quadrada.py
@@ -0,0 +1,10 @@
+# Dobro, Triplo, Raiz Quadrada.
+
+num = int(input('Digite um Número: '))
+dobro = num * 2
+triplo = num * 3
+rq = num ** (1/2)
+
+print(f'O Dobro de {num} é {dobro}\n'
+      f'O Triplo de {num} é {triplo}\n'
+      f'A Raiz Quadrada de {num} é {rq:.2f}')
