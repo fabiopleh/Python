cd19977b7bc6784f24082ebf4034d189b9997002
fabiopleh <fabiopleh@gmail.com>
2023-05-28 21:00:31 -0300

    Teste

---
+++ Exercicios/Exercicios_Curso_em_Video/ex007.py
@@ -0,0 +1,7 @@
+# Média Aritmética.
+
+n1 = float(input('Digite a Primeira nota: '))
+n2 = float(input('Digite a Segunda Nota: '))
+m = (n1 + n2) / 2
+
+print(f'A Média entre {n1} e {n2} é igual a {m:.1f}.')
