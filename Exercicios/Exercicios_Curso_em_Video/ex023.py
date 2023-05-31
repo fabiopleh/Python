1ba4aebf2c877d3d37d55bb256f2f07025a27a96
fabiopleh <fabiopleh@gmail.com>
2023-05-31 20:47:34 -0300

    Create ex023.py

---
+++ Exercicios/Exercicios_Curso_em_Video/ex023.py
@@ -0,0 +1,10 @@
+#Mostrando Unidade, Dezena, Centena e Milhar.
+num = int(input('Digite um Número entre 0 altura 9999: '))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print(f'Unidade: {u}')
+print(f'Dezena: {d}')
+print(f'Centena: {c}')
+print(f'Milhar: {m}')
