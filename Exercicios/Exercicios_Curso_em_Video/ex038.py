938a228c94cb77043f58807d4cc93ec023526551
fabiopleh <fabiopleh@gmail.com>
2023-06-01 22:20:53 -0300

    Create ex038.py

---
+++ Exercicios/Exercicios_Curso_em_Video/ex038.py
@@ -0,0 +1,10 @@
+# Verifica qual Número e Maior ou se são Igual.
+n1 = int(input('Digite o Primeiro Número: '))
+n2 = int(input('Digite o Segundo Número: '))
+
+if n1 > n2:
+    print('O Primeiro Número Digitado é o Maior.')
+elif n2 > n1:
+    print('O Segundo Número Digitado é o Maior.')
+else:
+    print('Os Dois Número são Iguais.')
