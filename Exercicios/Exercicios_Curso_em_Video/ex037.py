57c4460007a4c69b1c72fb62a252454093e4901c
fabiopleh <fabiopleh@gmail.com>
2023-06-01 22:20:50 -0300

    Create ex037.py

---
+++ Exercicios/Exercicios_Curso_em_Video/ex037.py
@@ -0,0 +1,17 @@
+# Convertendo Bases Numéricas.
+num = int(input('Digite um Número Inteiro: '))
+print('Para altura Base de Conversão:\n'
+'[ 1 ] - Binário\n'
+'[ 2 ] - Octual\n'
+'[ 3 ] - Hexadecimal')
+op = int(input('Digite sua Opção: '))
+
+if op == 1:
+    print(f'{num} Convertido para Binário é {bin(num)[2:]}')
+elif op == 2:
+    print(f'{num} Convertido para Octual é {oct(num)[2:]}')
+elif op == 3:
+    print(f'{num} Convertido para Hexadecimal é {hex(num)[2:]}')
+else:
+    print('Opção Inválida.')
+    
