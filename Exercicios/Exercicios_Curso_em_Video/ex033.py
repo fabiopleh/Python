26f723ed928be4c3243d9e26a6ac9549ded74df8
fabiopleh <fabiopleh@gmail.com>
2023-06-01 22:20:38 -0300

    Create ex033.py

---
+++ Exercicios/Exercicios_Curso_em_Video/ex033.py
@@ -0,0 +1,18 @@
+# Analiza o Maior e o Menor Número.
+n1 = int(input('Primeiro Valor: '))
+n2 = int(input('Segundo Valor: '))
+n3 = int(input('Terceiro Valor: '))
+
+menor = n1
+if n2 < n1 and n2 < n3:
+    menor = n2
+if n3 < n1 and n3 < n2:
+    menor = n3
+print(f'O Menor Valor Digitado Foi {menor}')
+
+maior = n1
+if n2 > n1 and n2 > n3:
+    maior = n2
+elif n3 > n1 and n3 > n2:
+    maior = n3
+print(f'O Maior Valor Digitado Foi {maior}')
